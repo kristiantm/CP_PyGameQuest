0f1eb1c3df6de02418e4d59340a70a23c3b04178
Kristian <61930466+kristiantm@users.noreply.github.com>
2025-04-02 17:37:37 +0200

    Seje ting

---
+++ Modul 1/02-examples/02-step-definition-of-game-window/main.py
@@ -0,0 +1,9 @@
+# Step 1
+import pygame
+
+pygame.init()
+
+# Step 2
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
---
+++ Modul 1/02-examples/03-step-initial-game-loop/main.py
@@ -0,0 +1,14 @@
+# Step 1
+import pygame
+
+pygame.init()
+
+# Step 2
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Step 3
+run = True
+while run:
+    ...
