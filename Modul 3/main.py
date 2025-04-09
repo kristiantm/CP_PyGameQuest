0f2e0a947ced67dab027b0341a3c2e572f296a0c
Kristian <61930466+kristiantm@users.noreply.github.com>
2025-04-09 18:21:45 +0200

    Module 3 - midt

--- Modul 3/main.py
+++ Modul 3/main.py
@@ -8,11 +8,22 @@ SCREE_HEIGHT = 600
 screen = pygame.display.set_mode((SCREE_WIDTH, SCREE_HEIGHT))
 
 
-# Step 1 - lav en Python klasse til din Sprite
+# Step 1 - Klasse til din Sprite - her et "Spaceship"
+class Link(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("./player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (400, 550)
+
 
 # Step 2 - Initialiser Sprite og Sprite Group
+link = Link()
+
+player_group = pygame.sprite.Group()
 
 # Step 3 - Tilføj Sprite til Sprite Group
+player_group.add(link)
 
 run = True
 while run:
@@ -20,6 +31,7 @@ while run:
     screen.fill((0, 0, 0))
 
     # Step 4 - kald "draw" metoden på din Sprite gruppe
+    player_group.draw(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
